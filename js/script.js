2599e3c8db0dad5bb24074dfecfa5e0e33e2cec4
Viktor <victorharasymiv97@gmail.com>
2024-12-02 23:18:23 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+function modalWindow() {
+  let modalWindow = document.querySelector('.modal_window');
+  modalWindow.classList.add("is-open");
+  }
+
+  function closeWindow() {
+    let modalWindow = document.querySelector('.modal_window');
+    modalWindow.classList.remove("is-open");
+    }
