6a6aa676f41b10bbc3509acec8aefc9f9de38c7d
Viktor <victorharasymiv97@gmail.com>
2024-12-03 02:12:42 +0100

    second

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+   //  M O D A L  F O R M
 
-function modalWindow() {
+    function modalWindow() {
   let modalWindow = document.querySelector('.modal_window');
   modalWindow.classList.add("is-open");
-  }
+     }
 
-  function closeWindow() {
+    function closeWindow() {
     let modalWindow = document.querySelector('.modal_window');
     modalWindow.classList.remove("is-open");
-    }
+     }
+
+
+    //  P H O N E  M E N U
+
+    function openPhoneMenu() {
+      let phoneMenu = document.querySelector('.phone_menu_section');
+      phoneMenu.classList.add("is-open");
+      }
+
+    function closePhoneMenu() {
+        let phoneMenu = document.querySelector('.phone_menu_section');
+        phoneMenu.classList.remove("is-open");
+      }
